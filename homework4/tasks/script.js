78e2725e174bebe76467186aede070d4cff5746c
Ruslan <someonefromua@gmail.com>
2020-04-04 13:02:36 +0300

    done + виправлення + написання по феншую

--- homework4/tasks/script.js
+++ homework4/tasks/script.js
@@ -5,7 +5,8 @@ function arrLog() {
 	let arr = [];
 	for (const argument of arguments) {
 		arr.push(argument);
-	}console.log(arr);
+	}
+	console.log(arr);
 
 }
 arrLog(1,3,5,7,3,7,8,3);
@@ -29,8 +30,6 @@ arrRandom(10,20,1);
 */
 
 
-
-
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
@@ -47,11 +46,6 @@ console.log(minNumber);
 */
 
 
-
-
-
-
-
 // - створити функцію яка приймає три числа та виводить та повертає найбільше.
 
 /*
@@ -63,12 +57,6 @@ function maxNum(a, b, c) {
 let maxNumber = maxNum(9,2,8);
 console.log(maxNumber);
 */
-
-
-
-
-
-
 
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше,
@@ -83,7 +71,8 @@ function maxAndMin(arr) {
         if (arguments[i] > max) {
             max = arguments[i];
         }
-    } console.log(`max - ${max}`);
+    }
+    console.log(`max - ${max}`);
     for (let i = 0; i < arguments.length; i++) {
         if (arguments[i] < max){
             max = arguments[i];
@@ -97,11 +86,6 @@ console.log(`min - ${min}`);
 
 
 */
-
-
-
-
-
 
 
 // - створити функцію яка виводить масив
@@ -139,12 +123,6 @@ console.log(`max - ${max}`);
 */
 
 
-
-
-
-
-
-
 // - створити функцію яка повертає найменьше число з масиву
 
 
@@ -167,7 +145,6 @@ console.log(`min - ${min}`);
 */
 
 
-
 // - створити функцію яка приймає масив чисел та складає значення елементів масиву та повертає його.
 /*
 let array = [1,0,3,2,6,8,5,8,1];
@@ -183,7 +160,6 @@ console.log(sum);
 */
 
 
-
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 /*
 let array = [4,7,3,7,8,3,3,66,2,7,3,8,1];
@@ -192,14 +168,14 @@ function medMath(array) {
     let medium;
     for (const number of array) {
         sum += number;
-    } medium = sum/array.length;
+    }
+    medium = sum/array.length;
     return(medium.toFixed(2))
 }
 
 let medium = medMath(array);
 console.log(medium);
 */
-
 
 
 // - Створити функцію яка приймає масив будь яких объектів, та повертає значення кількості об'єктів в
@@ -211,7 +187,8 @@ function numberOfEll(array) {
     let count = 0;
     for (const arrayKey in array) {
         count++;
-    }return(count)
+    }
+    return(count)
 }
 let count = numberOfEll(arr);
 console.log(count);
@@ -220,7 +197,6 @@ console.log(count);
 
 
 // - Створити функцію яка приймає масив будь яких объектів, та повертає загальн кількість полів в них
-
 
 
 /*
@@ -251,13 +227,6 @@ console.log(result);
 */
 
 
-
-
-
-
-
-
-
 // - створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //     Приклад
 //     [1,2,3,4]
@@ -275,7 +244,8 @@ function sumOfEllements(arr1, arr2) {
         let sum = arr1[i]+arr2[i];
         arr3.push(sum);
 
-    }return(arr3)
+    }
+    return(arr3)
 }
 let array3 = sumOfEllements(arr1, arr2);
 console.log(array3);
@@ -283,14 +253,10 @@ console.log(array3);
 */
 
 
-
-
-
-
-
 // - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
 
 /*
+
 function moveEllement(array, i) {
 let elemen = array.splice(i,1);
 
@@ -304,11 +270,6 @@ moveEllement(array, 6 );
 */
 
 
-
-
-
-
-
 // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 
 /*
@@ -319,7 +280,8 @@ function sortZero(array) {
             array.push(zero[0])
         }
         
-    }console.log(array);
+    }
+    console.log(array);
 }
 let array = [0,7,0,6,8,0,7,0,2,7,3,8,0,7,8,43,7,1];
 console.log(array);
@@ -333,13 +295,6 @@ sortZero(array);
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
 //
-
-
-
-
-
-
-
 
 
 // Створити функцію яка :
@@ -419,10 +374,6 @@ appendEllement(cars2, 'carslist');
 */
 
 
-
-
-
-
 // (на основі минулого ДЗ)
 // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
 // та повертає масив цих з'єднаних об'єктів.
@@ -459,10 +410,6 @@ console.log(newarr);
 */
 
 
-
-
-
-
 // ***- беремо завдання з правилами з укроку №3 :
 // Та робимо це функцією.При цьому правила отримувати через аргумент.
 // "Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
@@ -471,38 +418,38 @@ console.log(newarr);
 // 
 //
 let rules = [
-		{
-			title: 'Первое правило Бойцовского клуба.',
-			body: 'Никому не рассказывать о Бойцовском клубе.'
-		},
-		{
-			title: 'Второе правило Бойцовского клуба.',
-			body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-		},
-		{
-			title: 'Третье правило Бойцовского клуба.',
-			body: 'В схватке участвуют только двое.'
-		},
-		{
-			title: 'Четвертое правило Бойцовского клуба.',
-			body: 'Не более одного поединка за один раз.'
-		},
-		{
-			title: 'Пятое правило Бойцовского клуба.',
-			body: 'Бойцы сражаются без обуви и голые по пояс.'
-		},
-		{
-			title: 'Шестое правило Бойцовского клуба.',
-			body: 'Поединок продолжается столько, сколько потребуется.'
-		},
-		{
-			title: 'Седьмое правило Бойцовского клуба.',
-			body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-		},
-		{
-			title: 'Восьмое и последнее правило Бойцовского клуба.',
-			body: 'Новичок обязан принять бой.'
-		},
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
 
 ];
 
@@ -523,6 +470,113 @@ let block = createBlock(rule);
 wrap.appendChild(block);
 }
 */
+
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+/*
+let cars2 = [
+	{model: 'Tesla', year: 2015, power: 250, collor: 'black', driver: {name: 'ALina', age: 30, experience: 5}},
+	{model: 'A8', year: 2000, power: 300, collor: 'wjite', driver: {name: 'Oleg', age: 25, experience: 8}},
+	{model: 'Civik', year: 2019, power: 280, collor: 'grey', driver: {name: 'Bogdan', age: 22, experience: 2}},
+	{model: 'Charger', year: 2010, power: 250, collor: 'metalic', driver: {name: 'Rue', age: 28, experience: 5}},
+	{model: 'Padjero', year: 2009, power: 200, collor: 'black', driver: {name: 'Anna', age: 45, experience: 20}},
+	{model: 'Impresa', year: 2020, power: 310, collor: 'red', driver: {name: 'Vasya', age: 50, experience: 28}},
+	{model: 'GT', year: 2006, power: 230, collor: 'green', driver: {name: 'Petrik', age: 18, experience: 1}},
+	{model: 'Camaro', year: 2010, power: 300, collor: 'yellow', driver: {name: 'John', age: 16, experience: 0}},
+	{model: 'MSport', year: 2005, power: 200, collor: 'white', driver: {name: 'Karina', age: 29, experience: 5}},
+	{model: 'Cybertrack', year: 2020, power: 400, collor: 'grey', driver: {name: 'Jeck', age: 19, experience: 1}},
+];
+*/
+
+
+/*
+function cardCreator(array) {
+	let card = document.createElement("div");
+	card.classList.add('car');
+	for (const arrayKey in array) {
+		let specification = document.createElement("div");
+		if (typeof array[arrayKey] === 'object'){
+			let include = document.createElement("div");
+			include.innerText = `${arrayKey}`;
+			include.classList.add('include');
+			card.appendChild(include);
+			for (const arrayElementKey in array[arrayKey]) {
+				let specincule = document.createElement("div");
+				specincule.innerText = `${arrayElementKey} - ${array[arrayKey][arrayElementKey]}`;
+				include.appendChild(specincule)
+			}
+		}else  specification.innerText = `${arrayKey} - ${array[arrayKey]}`;
+		card.appendChild(specification);
+
+	} return(card);
+}
+function appendEllement(array, identify){
+	let elementId = document.getElementById(identify);
+	for (const arrayElement of array) {
+		let card = cardCreator(arrayElement);
+		elementId.appendChild(card);
+	}
+}
+
+appendEllement(cars2, 'carslist');*/
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////Універсальний ітератор//////////////////////
+////////////////////////////////////////////////////////////////
+
+/*
+function iteratorMass(array) {
+	for (const arrayElement of array) {
+		console.log(arrayElement);
+	}
+}
+function iterator3000(array) {
+	for (const arrayKey in array) {
+		if (typeof array[arrayKey]==='object' && !Array.isArray(array[arrayKey])) {
+			console.log(arrayKey);
+			iterator3000(array[arrayKey]);
+			}else if (Array.isArray(array[arrayKey])){
+			iteratorMass(array[arrayKey])
+		}else console.log(`${arrayKey}	- ${array[arrayKey]}`);
+		}
+	}
+iterator3000(mass);
+*/
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
